288e5d008ec47f5875d3c8154595f3c65af7d45d
Purpur <pemigrade@gmail.com>
2023-10-26 12:01:42 -0400

    hands logic

--- controller/controller.ino
+++ controller/controller.ino
@@ -2,21 +2,24 @@
 #include <WiFiUdp.h>
 #include <math.h>
 #include<Wire.h>
+
 const int MPU_addr=0x68;  // I2C address of the MPU-6050, Plug SDA 21, SCL 22.
 int16_t AcX,AcY,AcZ;
 // int16_t Tmp,GyX,GyY,GyZ; // not using these, but could...
 
-const int touchPin = 32;
+const int touchPin = 32; // nose touch sensor
+const int handsPlacedPin = 33; // hands placed sensor
 
 const char* ssid = "yale wireless";
 
-const char* udpServerIP = "172.27.33.77";
+const char* udpServerIP = "172.26.91.231";
 const int udpServerPort = 12345;
 
 WiFiUDP udp;
 
 void setup() {
   pinMode(touchPin, INPUT);
+  pinMode(handsPlacedPin, INPUT_PULLUP);
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);  // PWR_MGMT_1 register
@@ -35,20 +38,21 @@ void setup() {
 
 void loop() {
   double touch = analogRead(touchPin) / 4096.0;
+  int hands = digitalRead(handsPlacedPin);
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,6,true);  // read 6 registers. Change to 14 for:
+  Wire.requestFrom((uint16_t)MPU_addr, (uint8_t)6,true);  // read 6 registers. Change to 14 for:
                                       // 6 accel, 2 temp, 6 gyro
   AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
   AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
 
-  float pitch = 0.5 + (atan2(AcY, sqrt(AcX*AcX + AcZ*AcZ)) / PI);
-  float roll = 0.5 - (atan2(AcX, sqrt(AcY*AcY + AcZ*AcZ)) / PI);
+  float pitch = 0 + (atan2(AcX, sqrt(AcY*AcY + AcZ*AcZ)) / PI);
+  float roll = 0 - (atan2(AcY, sqrt(AcX*AcX + AcZ*AcZ)) / PI);
 
-  char buffer[50];
-  sprintf(buffer, "Pitch: %.4f, Yaw: %.4f, Touch: %.4f", pitch, yaw, touch);
+  char buffer[60];
+  sprintf(buffer, "Pitch: %.4f, Roll: %.4f, Touch: %.4f, Hands: %d", pitch, roll, touch, hands);
 
   Serial.println(buffer);
   udp.beginPacket(udpServerIP, udpServerPort);
