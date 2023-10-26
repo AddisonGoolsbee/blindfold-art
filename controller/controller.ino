5529bacb7a9fd655541c6fea9c05b9fedf954ed8
Purpur <pemigrade@gmail.com>
2023-10-26 10:58:51 -0400

    don't request unnecessary registers

--- controller/controller.ino
+++ controller/controller.ino
@@ -39,8 +39,8 @@ void loop() {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers,
-                                       // 6 accel, 2 temp, 6 gyro. using accel.
+  Wire.requestFrom(MPU_addr,6,true);  // read 6 registers. Change to 14 for:
+                                      // 6 accel, 2 temp, 6 gyro
   AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
   AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
